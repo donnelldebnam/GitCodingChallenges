0acaa898cbea0aa82bc2f30c06a7f5cdf9de652b
donnelldebnam <donnedebnam1@aol.com>
2020-02-17 17:38:37 -0500

    Test files

---
+++ src/Exercise01.java
@@ -0,0 +1,19 @@
+public class Exercise01 {
+
+	public static void main(String[] args) {
+
+		// Test bytes
+		byte a = 010;
+		byte b = 101;
+
+		// "Add" function
+		byte sum = add(a, b);
+		System.out.println("This sum of bytes a and b is: " + sum);
+
+	}
+
+	static int add(byte a, byte b) {
+		return (byte)(a + b);
+	}
+
+}
