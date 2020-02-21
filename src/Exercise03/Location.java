48cc2ab32e8043e34035d07ece2889d14fccdecc
AdrianTJenkins <adrian.jenkins99@gmail.com>
2020-02-20 19:06:39 -0500

    Completed the commented tasks and added getters and setters for attributes of Vechicle and Location

--- src/Exercise03/Location.java
+++ src/Exercise03/Location.java
@@ -5,16 +5,36 @@
 public class Location {
 
     private long latitude = 0;
-    private long longititude = 0;
+    private long longitude = 0;
 
     public Location(long latitude, long longititude) {
         this.latitude = latitude;
         this.longititude = longititude;
     }
 
+    public long getLatitude() {
+        return latitude;
+    }
+
+    public long getLongitude() {
+        return longitude;
+    }
+
+    public void setLatitude(long lat) {
+        latitude = lat;
+    }
+
+    public void setLongitude(long lon) {
+        longitude = lon;
+    }
+
     /**
      * Task 2: Add a function here to print location in the format of 
      * "(latitude, longitude)" (Hint: toString)
      */
+
+     public void printLocation() {
+        System.out.println("(" + latitude + ", " + longitude + ")");
+     }
 
 }
