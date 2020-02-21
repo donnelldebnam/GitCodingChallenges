4b5b08f377bc0b8b12a556f8aca0d6d18f9dbb1a
Cymmone-Yancey <53952302+Cymmone-Yancey@users.noreply.github.com>
2020-02-20 19:33:08 -0500

    completed commented tasks

--- src/Exercise03/Vehicle.java
+++ src/Exercise03/Vehicle.java
@@ -3,28 +3,34 @@
  * of the class and have the option to change the license plates, drive,
  * check the milage, and find our location via the GPS.
  */
+ import java.lang.Math;
+ 
 public class Vehicle {
 
-	private long milage = 0;
-	private String licensePlate = "";
+	protected long milage = 0;
+	protected String licensePlate = "";
 	private String model = "";
-	private Location location;
+	protected Location location;
 
 	public Vehicle(String model, String licensePlate, long milage) {
 		this.model = model;
 		this.licensePlate = licensePlate;
 		this.milage = milage;
+      location = new Location(0,0);
 	}
 
-	void drive(long latitude, long longitude) {
+	public void drive(long latitude, long longitude) {
 		// Task 1: Implement a function that uses the old latitude and longitude
 		// to account for the amount of new milage. Use the value to then update
-		// the previous milage.
-		location = new Location(latitude, longitude);
+		// the previous milage. sqrt (x2 - x1)^2 + (y2 - y1)^2
+
+      milage += (long)Math.sqrt(Math.pow(this.location.getLatitude() - latitude, 2) + Math.pow(this.location.getLongitude() - longitude, 2));
+		location.setLatitude(latitude);
+      location.setLongitude(longitude);
 	}
 
 	void changePlates(String lplate) {
-		this.licensePlate = licensePlate;
+		this.licensePlate = lplate;
 	}
 
 	long getMilage() {
