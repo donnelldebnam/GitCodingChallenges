e2d44929492c9738f52f410015d7ee934c7fa0bb
Yianni PM <51059714+mypablo@users.noreply.github.com>
2020-03-02 21:31:21 +0200

    Update SpaceshipTest.java

--- src/Day 2/Exercise/test/com/exercise/SpaceshipTest.java
+++ src/Day 2/Exercise/test/com/exercise/SpaceshipTest.java
@@ -14,12 +14,34 @@ public class SpaceshipTest {
         assertEquals(0, testSpeed,0);
     }
 
-    // Task 1: Implement tests for getLocation()
+    public void testLocation() {
+        // Task 1: Implement tests for getLocation()
+        Spaceship test = new Spaceship(new Location(5,405435885));
+        Location testLocation = test.getLocation();
 
-    // Task 2: Implement tests for liftOff()
 
-    // Task 3: Implement tests for enterSpace()
+    }
 
-    // Task 4: Implement tests for inOrbit()
+        public void testLiftOff() {
+            // Task 2: Implement tests for liftOff()
+        Spaceship test = new Spaceship(new Location(4,435956838));
+          test.liftOff();
+            
+    }
+        public void enterSpace() {
+            // Task 3: Implement tests for enterSpace()
+            
+            Spaceship test = new Spaceship(new Location(3,569384529));
+            test.enterSpace();
+            
+            
+        }
+        public void inOrbit() {
+            // Task 4: Implement tests for inOrbit()
+            Spaceship test = new Spaceship(new Location(2,234923869));
+            boolean testinOrbit = test.inOrbit();
+            
+            
+        }
 
 }
